b3d83975946adafece830466f89cc2487b166183
Matthew Sherman <msherman0801@gmail.com>
2018-10-11 02:48:41 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,20 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)  
+  if speed > 60
+    true
+  else if speed < 40
+    true
+  else
+    false
+end
+end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 ? true : (speed < 40 ? true : false)
 end
 	
 
